7679a45d81c69dc0f95bbee7c95612005e4f28d2
peoray <peoray@yahoo.com>
2020-09-22 01:05:54 +0100

    Updates

---
+++ precache-manifest.6b0131bcc16b50b5745819cd16f80e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f48309288fd2b2c702a701aaaee70f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eeb4701c36f42b081e4",
+    "url": "/static/css/main.61da7730.chunk.css"
+  },
+  {
+    "revision": "b2507dad690fadd5baec",
+    "url": "/static/js/2.9b777baa.chunk.js"
+  },
+  {
+    "revision": "0eeb4701c36f42b081e4",
+    "url": "/static/js/main.94f29ccb.chunk.js"
+  },
+  {
+    "revision": "1d5c5dfca3c716784b6c",
+    "url": "/static/js/runtime-main.3f1d1d89.js"
+  }
+]);
