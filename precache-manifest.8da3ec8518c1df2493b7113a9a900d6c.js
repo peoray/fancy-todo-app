797b324dd7ad10ff27a25e978649912dd3a962bd
peoray <peoray@yahoo.com>
2020-09-22 01:18:10 +0100

    Updates

---
+++ precache-manifest.8da3ec8518c1df2493b7113a9a900d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec06a0871e541ab51a1ca3a13db070d",
+    "url": "/fancy-todo-app/index.html"
+  },
+  {
+    "revision": "a735adb6bcac279d61a2",
+    "url": "/fancy-todo-app/static/css/main.61da7730.chunk.css"
+  },
+  {
+    "revision": "b2507dad690fadd5baec",
+    "url": "/fancy-todo-app/static/js/2.9b777baa.chunk.js"
+  },
+  {
+    "revision": "a735adb6bcac279d61a2",
+    "url": "/fancy-todo-app/static/js/main.200d3c45.chunk.js"
+  },
+  {
+    "revision": "9963001c05ebdb080385",
+    "url": "/fancy-todo-app/static/js/runtime-main.cc1203be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b0131bcc16b50b5745819cd16f80e14.js"
+  "/fancy-todo-app/precache-manifest.8da3ec8518c1df2493b7113a9a900d6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fancy-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
